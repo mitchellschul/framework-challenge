97af99ae6e2dc183712e878af1bcba6a49271ba8
Mitch <70587519+mitchellschul@users.noreply.github.com>
2023-02-19 20:45:54 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import batman from './images/batman-icon.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='flex flex-col h-screen items-center justify-center'>
+        <div className='text-2xl font-varela-round text-gray-500'>Oh snap Batman, we've encountered a</div>
+        <div className='flex flex-row items-center'>
+          <h1 className='text-9xl mr-2 font-varela-round'>4</h1>
+          <img src={batman} alt='batman' className='w-40' />
+          <h1 className='text-9xl ml-2 font-varela-round'>4</h1>
+        </div>
+
+        <div className='text-2xl font-varela-round text-gray-500'>The page you are looking for does not exist...</div>
+        <div className='mt-5 p-2 text-gray-100 cursor-pointer font-varela-round bg-gray-500 rounded-md hover:bg-gray-400 hover:scale-105 transition'>Back to the Batcave</div>
+      </div>
     </div>
+
   );
 }
 
